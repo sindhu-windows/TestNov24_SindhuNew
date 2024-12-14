e23da0d67ad3a5f3e58bea98f4ed9a8718e6179f
sindhu-windows <sindhupatil32@gmail.com>
2024-12-14 11:40:07 +0530

    Branch B 2nd changes

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -2,7 +2,12 @@ package practice;
 
 public class PracClass1 {
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("Hello_class1, Branch A changes, second change on Bransh A.");
 		System.out.println("Branch A changes.");
+=======
+		System.out.println("Hello_class1, Branch A changes.");
+		System.out.println("Branch B changes, 2nd changes to Branch B");
+>>>>>>> Stashed changes
 	}
 }
