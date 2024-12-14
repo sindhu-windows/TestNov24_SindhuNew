fe110e38bf311366133365e9e4db40e531b54ce6
sindhu-windows <sindhupatil32@gmail.com>
2024-12-14 12:07:29 +0530

    scenario 2

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -2,7 +2,6 @@ package practice;
 
 public class PracClass1 {
 	public static void main(String[] args) {
-		System.out.println("Hello_class1, Branch A changes, second change on Bransh A.");
-		System.out.println("Branch B changes, 2nd changes to Branch B");
-	}
+		System.out.println("Hello_class1, Branch A 2nd scenario");
+		}
 }
