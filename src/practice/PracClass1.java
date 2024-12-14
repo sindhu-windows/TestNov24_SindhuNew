837aae8296719167d16a2749bc4096b0f6904202
sindhu-windows <sindhupatil32@gmail.com>
2024-12-14 11:17:10 +0530

    Conflict resolved

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -3,5 +3,6 @@ package practice;
 public class PracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1, Branch A changes.");
+		System.out.println("Branch A changes.");
 	}
 }
