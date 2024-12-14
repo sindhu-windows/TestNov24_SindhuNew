54daf29f193a826e53a092c37e7286b621ba1f6d
sindhu-windows <sindhupatil32@gmail.com>
2024-12-14 08:58:10 +0530

    New Project files Upload

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class PracClass1 {
+//import practice.Divisible5_3;
 
+public class PracClass1 {
+	void printDivisible5_3(int startRange, int endRange){
+		System.out.println("Numbers divisible by 5 and 3:");
+		for(int num=startRange;num<=endRange;num++){
+			if(num%5==0 && num%3==0){
+				System.out.println(num);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		PracClass1 divisible5_3 = new PracClass1();
+		divisible5_3.printDivisible5_3(5,40);
+	}
 }
--- src/practice/PracClass2.java
+++ src/practice/PracClass2.java
@@ -1,5 +1,16 @@
 package practice;
 
-public class PracClass2 {
+//import basic.Incremental;
 
+public class PracClass2 {
+	public void postInc(){
+		int num = 1;
+		num++;
+		System.out.println(num);
+		System.out.println(num);
+	}
+	public static void main(String[] args){
+		PracClass2 incremental = new PracClass2();
+		incremental.postInc();
+	}
 }
